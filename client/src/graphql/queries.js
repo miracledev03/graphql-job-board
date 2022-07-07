695466a9b37cf2bd611451fabd69ca8316a033e1
unknown <miracledev0303@gmail.com>
2022-07-07 15:32:52 -0300

    company query

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -41,3 +41,22 @@ export async function getJobs() {
 
   return jobs;
 }
+
+export async function getCompany(id) {
+  const query = gql`
+    query CompanyQuery($id: ID!) {
+      company(id: $id) {
+        name
+        description
+      }
+    }
+  `;
+
+  const variables = { id };
+
+  const { company } = await request(GRAPHQL_URL, query, variables);
+
+  console.log(company);
+
+  return company;
+}
