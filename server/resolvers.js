68091b5f57bf7a93345dcc8a72584c4443193d0b
unknown <miracledev0303@gmail.com>
2022-07-08 15:24:36 -0300

    object graph navigation

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,11 +4,16 @@ export const resolvers = {
   Query: {
     job: (_root, { id }) => Job.findById(id),
     jobs: () => Job.findAll(),
+    company: (_root, { id }) => Company.findById(id),
   },
 
   Job: {
     company: (job) => {
       return Company.findById(job.companyId);
     },
+  },
+
+  Company: {
+    jobs: (company) => Job.findAll((job) => job.companyId === company.id),
   },
 };
